7b9146da6b126b7ce2f16e23deac82010cf1ef4f
brahmbhatt <margibrahmbhatt13@gmail.com>
2018-02-08 01:01:25 +0530

    testcase added for unsuccessful post request for password and username

--- tests/routes/loginvalidate.test.js
+++ tests/routes/loginvalidate.test.js
@@ -5,7 +5,7 @@ describe('check server response code', () => {
     const request = {
       method: 'POST',
       url: '/login/validation',
-      payload: JSON.stringify({ username: 'Margi1013' }),
+      payload: JSON.stringify({ username: 'Margi_1013', password: '3ngv4@-_cmlve' }),
     };
     server.inject(request, (response) => {
       expect(response.statusCode).toBe(200);
@@ -13,11 +13,47 @@ describe('check server response code', () => {
     });
   });
 
-  test('Test for unsuccessful POST request reply code from the server', (done) => {
+  test('Test for unsuccessful POST request because username length must be atleat 5 char', (done) => {
     const request = {
       method: 'POST',
       url: '/login/validation',
-      payload: JSON.stringify({ username: '90m' }),
+      payload: JSON.stringify({ username: '90m', password: 'ngvwcmlve' }),
+    };
+    server.inject(request, (response) => {
+      expect(response.statusCode).toBe(400);
+      done();
+    });
+  });
+
+  test('Test for unsuccessful POST request because username length must be atmost 15 char', (done) => {
+    const request = {
+      method: 'POST',
+      url: '/login/validation',
+      payload: JSON.stringify({ username: 'abcd efghijklmnopqrstuv', password: 'ngvwcmlve' }),
+    };
+    server.inject(request, (response) => {
+      expect(response.statusCode).toBe(400);
+      done();
+    });
+  });
+
+  test('Test for unsuccessful POST request because password length must be atleast 4 char', (done) => {
+    const request = {
+      method: 'POST',
+      url: '/login/validation',
+      payload: JSON.stringify({ username: 'abcdefghijklmnopqrstuv', password: 'ng' }),
+    };
+    server.inject(request, (response) => {
+      expect(response.statusCode).toBe(400);
+      done();
+    });
+  });
+
+  test('Test for unsuccessful POST request because password length must be atmost 20 char', (done) => {
+    const request = {
+      method: 'POST',
+      url: '/login/validation',
+      payload: JSON.stringify({ username: 'abcdefghijklmnopqrstuv', password: 'nghjkjtdvhgfty123456#@cfyvlubnmnjbhvcfg' }),
     };
     server.inject(request, (response) => {
       expect(response.statusCode).toBe(400);

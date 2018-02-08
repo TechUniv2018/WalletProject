792a92c5742b688f9a43e516a9a5cb349e0e2902
brahmbhatt <margibrahmbhatt13@gmail.com>
2018-02-08 11:04:50 +0530

    testcase added to validate aadhar number

--- tests/routes/registerValidation.test.js
+++ tests/routes/registerValidation.test.js
@@ -42,4 +42,32 @@ describe('check server response code', () => {
       done();
     });
   });
+
+  test('Test for unsuccessful POST request because aadhar number can not be string', (done) => {
+    const request = {
+      method: 'POST',
+      url: '/login/validation',
+      payload: JSON.stringify({
+        username: 'Margi_1013', password: '3ngv4@-_cmlve', firstName: 'margi', lastName: 'brahmbhatt', aadharNo: '123456789012', phone: '8141165366', accountNo: '123456789012',
+      }),
+    };
+    server.inject(request, (response) => {
+      expect(response.statusCode).toBe(400);
+      done();
+    });
+  });
+
+  test('Test for unsuccessful POST request because aadhar number mustbe of 12 digits', (done) => {
+    const request = {
+      method: 'POST',
+      url: '/login/validation',
+      payload: JSON.stringify({
+        username: 'Margi_1013', password: '3ngv4@-_cmlve', firstName: 'margi', lastName: 'brahmbhatt', aadharNo: 12345678901, phone: '8141165366', accountNo: '123456789012',
+      }),
+    };
+    server.inject(request, (response) => {
+      expect(response.statusCode).toBe(400);
+      done();
+    });
+  });
 });

5660749b65c4ef5ee9d91b49141076729c1882fa
Shoaib <ahamedshoaibk@gmail.com>
2018-02-08 10:28:44 +0530

    added firstName and lastName field to user Details table

---
+++ migrations/20180208043128-add-names-field-in-user-deatails.js
@@ -0,0 +1,11 @@
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    queryInterface.addColumn('userDetails', 'firstName', Sequelize.STRING);
+    queryInterface.addColumn('userDetails', 'lastName', Sequelize.STRING);
+  },
+
+  down: (queryInterface, Sequelize) => {
+    queryInterface.removeColumn('userDetails', 'firstName');
+    queryInterface.removeColumn('userDetails', 'lastName');
+  },
+};

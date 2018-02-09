7850a02fb035ec0f29a4e87e955df366eb47caba
brahmbhatt <margibrahmbhatt13@gmail.com>
2018-02-09 10:40:26 +0530

    fixed testcases

--- tests/routes/registerValidation.test.js
+++ tests/routes/registerValidation.test.js
@@ -43,6 +43,20 @@ describe('check server response code', () => {
     });
   });
 
+  test('Test for unsuccessful POST request because aadhar number can not be string', (done) => {
+    const request = {
+      method: 'POST',
+      url: '/register/validation',
+      payload: JSON.stringify({
+        userName: 'Margi_1013', password: '3ngv4@-_cmlve', firstName: 'margi', lastName: 'brahmbhatt', aadharNo: '12345M6789012', phone: '8141165366', accountNo: '123456789012',
+      }),
+    };
+    server.inject(request, (response) => {
+      expect(response.statusCode).toBe(400);
+      done();
+    });
+  });
+
 
   test('Test for unsuccessful POST request because phone number must only have numbers', (done) => {
     const request = {

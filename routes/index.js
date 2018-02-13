156828eee0ccf620c8d315285bdea4635fb1a682
lakhara <siddharth.lakhara@gmail.com>
2018-02-13 18:07:19 +0530

    Modified config JSON for local

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,7 @@
 const ping = require('./ping');
 const { route: userRegistration } = require('./userRegister');
 const userLogin = require('./userLogin');
+const rcvMoney = require('./rcv');
+const approve = require('./approve');
 
-module.exports = [].concat(ping, userRegistration, userLogin);
+module.exports = [].concat(ping, userRegistration, userLogin, rcvMoney, approve);

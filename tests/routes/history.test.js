b0ff14e5032696391517fc7dff21ed9d1eb99b7d
Shoaib <ahamedshoaibk@gmail.com>
2018-02-22 12:07:34 +0530

    completed tests for history

--- tests/routes/history.test.js
+++ tests/routes/history.test.js
@@ -31,7 +31,7 @@ describe('check server response code', () => {
 });
 
 describe('check server response message', () => {
-  test('/transactions/history should respond with status code 200 when credentials are provided', (done) => {
+  test('/transactions/history should respond with array of transactions', (done) => {
     server.inject({
       method: 'GET',
       url: '/transactions/history',
@@ -41,18 +41,48 @@ describe('check server response message', () => {
       },
 
     }, (response) => {
-      expect(response.payload).toBe(200);
+      const { history } = JSON.parse(response.payload);
+      expect.assertions(history.length * 9);
+      history.forEach((transaction) => {
+        expect(transaction.transactionId).toBeDefined();
+        expect(transaction.fromId).toBeDefined();
+        expect(transaction.toId).toBeDefined();
+        expect(transaction.amount).toBeDefined();
+        expect(transaction.reason).toBeDefined();
+        expect(transaction.status).toBeDefined();
+        expect(transaction.timeStamp).toBeDefined();
+        expect(transaction.category).toBeDefined();
+        expect(transaction.type).toBeDefined();
+      });
+      done();
+    });
+  });
+
+  test('/transactions/history should respond with transactions with fromId or toId equal to userId', (done) => {
+    const userId = 1;
+
+    const isValid = (transaction) => {
+      if (transaction.fromId === userId || transaction.toId === userId) {
+        return true;
+      }
+      return false;
+    };
+
+    server.inject({
+      method: 'GET',
+      url: '/transactions/history',
+      credentials: {
+        userId,
+        userName: 'John_Doe',
+      },
+
+    }, (response) => {
+      const { history } = JSON.parse(response.payload);
+      expect.assertions(history.length);
+      history.forEach((transaction) => {
+        expect(isValid(transaction)).toBe(true);
+      });
       done();
     });
   });
 });
-
-
-// describe('check server response message', () => {
-//   test('/ping should respond message pong', (done) => {
-//     server.inject('/ping', (response) => {
-//       expect(response.result).toBe('pong');
-//       done();
-//     });
-//   });
-// });

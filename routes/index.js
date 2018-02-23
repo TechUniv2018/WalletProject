d648444265da279520c204826162bf60f35f73ba
Shoaib <ahamedshoaibk@gmail.com>
2018-02-23 14:06:56 +0530

    route and validation and swagger specs complete

--- routes/index.js
+++ routes/index.js
@@ -3,5 +3,6 @@ const userRegistration = require('./userRegister');
 const userLogin = require('./userLogin');
 const auth = require('./auth');
 const history = require('./history');
+const catagory = require('./catagory');
 
-module.exports = [].concat(ping, userRegistration, userLogin, auth, history);
+module.exports = [].concat(ping, userRegistration, userLogin, auth, history, catagory);

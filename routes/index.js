601b95ed851f3aedefd1146b6cabcbaf91b76924
lakhara <siddharth.lakhara@gmail.com>
2018-03-18 18:07:17 +0530

    updating with socket-send

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,8 @@
 const ping = require('./ping');
 const userRegistration = require('./userRegister');
 const userLogin = require('./userLogin');
-const rcvMoney = require('./rcv');
 const approve = require('./approve');
 const auth = require('./auth');
 const history = require('./history');
 
-module.exports = [].concat(ping, userRegistration, userLogin, rcvMoney, approve, auth, history);
+module.exports = [].concat(ping, userRegistration, userLogin, approve, auth, history);

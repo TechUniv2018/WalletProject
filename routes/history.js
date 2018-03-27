30cc9f98d5794799d3256a8a47931c456f73e25b
Shoaib <ahamedshoaibk@gmail.com>
2018-03-27 23:34:04 +0530

    split working

--- routes/history.js
+++ routes/history.js
@@ -29,8 +29,34 @@ const getHistory = id => models.transactions.findAll({
   });
 });
 
+const getSplitableHistory = id => models.transactions.findAll({
+  attributes: ['transactionId', 'fromId', 'toId', 'amount', 'reason', 'status', 'timeStamp', 'category', 'type'],
+  where: {
+    fromId: id,
+    status: 'COMPLETED',
+    type: 1,
+  },
+}).then((result) => {
+  const resultArrPromise = [];
+  const detailsArr = [];
+  result.forEach((transaction) => {
+    detailsArr.push(transaction.get({ plain: true }));
+    resultArrPromise.push(models.users.findOne({ where: { userId: transaction.fromId } }));
+    resultArrPromise.push(models.users.findOne({ where: { userId: transaction.toId } }));
+  });
 
-module.exports = {
+  return Promise.all(resultArrPromise).then((resultArr) => {
+    for (let transactionNo = 0; transactionNo < result.length; transactionNo += 1) {
+      detailsArr[transactionNo].fromUser = resultArr[transactionNo * 2].userName;
+      detailsArr[transactionNo].toUser = resultArr[(transactionNo * 2) + 1].userName;
+    }
+
+    return detailsArr;
+  });
+});
+
+
+module.exports = [{
   method: 'GET',
   path: '/transactions/history',
   config: {
@@ -53,4 +79,27 @@ module.exports = {
       });
     });
   },
-};
+}, {
+  method: 'GET',
+  path: '/transactions/history/splitable',
+  config: {
+    auth: 'jwt',
+    tags: ['api'],
+    description: 'Get Transaction history for  current user',
+    notes: 'Get All transaction history for current user',
+    plugins: {
+      'hapi-swagger': historySwagger,
+    },
+    validate: {
+      headers: historyHeaderValidation,
+    },
+  },
+  handler: (request, response) => {
+    getSplitableHistory(request.auth.credentials.userId).then((history) => {
+      response({
+        statusCode: 200,
+        history,
+      });
+    });
+  },
+}];
